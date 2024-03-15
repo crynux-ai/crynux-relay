809027a19713d5031e285a53a37f0b9391db71fa
iwehf <henry.lee@crynux.ai>
2024-03-15 10:30:35 +0800

    add gen NetworkStats contract abi script

--- scripts/gen-abi-binding.sh
+++ scripts/gen-abi-binding.sh
@@ -3,3 +3,4 @@ abigen --abi ../blockchain/abi/Node.abi --pkg bindings --type Node --out ../bloc
 abigen --abi ../blockchain/abi/CrynuxToken.abi --pkg bindings --type CrynuxToken --out ../blockchain/bindings/CrynuxToken.go
 abigen --abi ../blockchain/abi/QOS.abi --pkg bindings --type QOS --out ../blockchain/bindings/QOS.go
 abigen --abi ../blockchain/abi/TaskQueue.abi --pkg bindings --type TaskQueue --out ../blockchain/bindings/TaskQueue.go
+abigen --abi ../blockchain/abi/NetworkStats.abi --pkg bindings --type NetworkStats --out ../blockchain/bindings/NetworkStats.go
