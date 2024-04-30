43a556ff183c8cb686ad6ef6e6b64cce32854d51
iwehf <henry.lee@crynux.ai>
2024-04-30 17:31:02 +0800

    remove crynux token in gen-abi-binding scripts

--- scripts/gen-abi-binding.sh
+++ scripts/gen-abi-binding.sh
@@ -1,6 +1,5 @@
 abigen --abi ../blockchain/abi/Task.abi --pkg bindings --type Task --out ../blockchain/bindings/Task.go
 abigen --abi ../blockchain/abi/Node.abi --pkg bindings --type Node --out ../blockchain/bindings/Node.go
-abigen --abi ../blockchain/abi/CrynuxToken.abi --pkg bindings --type CrynuxToken --out ../blockchain/bindings/CrynuxToken.go
 abigen --abi ../blockchain/abi/QOS.abi --pkg bindings --type QOS --out ../blockchain/bindings/QOS.go
 abigen --abi ../blockchain/abi/TaskQueue.abi --pkg bindings --type TaskQueue --out ../blockchain/bindings/TaskQueue.go
 abigen --abi ../blockchain/abi/NetworkStats.abi --pkg bindings --type NetworkStats --out ../blockchain/bindings/NetworkStats.go
