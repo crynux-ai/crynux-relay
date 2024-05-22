f6e0a4a62d2d3644740fc3e7b51062d1ebf1d9e6
iwehf <henry.lee@crynux.ai>
2024-05-22 16:05:41 +0800

    change migration 20240518

--- migrate/migrations/m_20240518.go
+++ migrate/migrations/m_20240518.go
@@ -1,33 +1,37 @@
 package migrations
 
 import (
-	"crynux_relay/models"
-
 	"github.com/go-gormigrate/gormigrate/v2"
 	"gorm.io/gorm"
 )
 
 func M20240518(db *gorm.DB) *gormigrate.Gormigrate {
-	
+	type InferenceTask struct {
+		TaskId uint64 `json:"task_id" gorm:"index"`
+	}
+
+	type SelectedNode struct {
+		InferenceTaskID uint `gorm:"index"`
+	}
 
 	return gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
 		{
 			ID: "M20240518",
 			Migrate: func(tx *gorm.DB) error {
 
-				if err := tx.Migrator().CreateIndex(&models.InferenceTask{}, "TaskId"); err != nil {
+				if err := tx.Migrator().CreateIndex(&InferenceTask{}, "TaskId"); err != nil {
 					return err
 				}
 
-				return tx.Migrator().CreateIndex(&models.SelectedNode{}, "InferenceTaskID")
+				return tx.Migrator().CreateIndex(&SelectedNode{}, "InferenceTaskID")
 			},
 			Rollback: func(tx *gorm.DB) error {
 
-				if err := tx.Migrator().DropIndex(&models.InferenceTask{}, "TaskId"); err != nil {
+				if err := tx.Migrator().DropIndex(&InferenceTask{}, "TaskId"); err != nil {
 					return err
 				}
 
-				return tx.Migrator().DropIndex(&models.SelectedNode{}, "InferenceTaskID")
+				return tx.Migrator().DropIndex(&SelectedNode{}, "InferenceTaskID")
 			},
 		},
 	})
