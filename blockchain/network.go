9217d6b466d1bba401fc6b89712e17d3a95c7098
iwehf <henry.lee@crynux.ai>
2024-07-08 19:10:42 +0800

    limit concurrency in GetAllNodesData

--- blockchain/network.go
+++ blockchain/network.go
@@ -3,10 +3,11 @@ package blockchain
 import (
 	"context"
 	"math/big"
-	"sync"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	log "github.com/sirupsen/logrus"
+
 )
 
 func GetAllNodesNumber() (busyNodes *big.Int, allNodes *big.Int, activeNodes *big.Int, err error) {
@@ -115,23 +116,30 @@ func GetAllNodesData(startIndex, endIndex int) ([]NodeData, error) {
 
 	nodeData := make([]NodeData, len(allNodeInfos))
 
-	var wg sync.WaitGroup
+	errCh := make(chan error, len(allNodeInfos))
+	// limit concurrency goroutines count
+	limiter := make(chan struct{}, 4)
 
 	for idx, nodeInfo := range allNodeInfos {
-		wg.Add(1)
-
 		nodeData[idx] = NodeData{
 			Address:   nodeInfo.NodeAddress.Hex(),
 			CardModel: nodeInfo.GPUModel,
 			VRam:      int(nodeInfo.VRAM.Int64()),
+			Balance: big.NewInt(0),
 		}
 
+		limiter <- struct{}{}
 		go func(idx int, nodeAddress common.Address) {
-			defer wg.Done()
+
+			defer func() {
+				<-limiter
+			}()
 
 			balance, err := client.BalanceAt(context.Background(), nodeAddress, nil)
 
 			if err != nil {
+				log.Errorf("get wallet balance error: %v", err)
+				errCh <- err
 				return
 			}
 
@@ -140,6 +148,8 @@ func GetAllNodesData(startIndex, endIndex int) ([]NodeData, error) {
 				Context: context.Background(),
 			}, nodeAddress)
 			if err != nil {
+				log.Errorf("get node status error: %v", err)
+				errCh <- err
 				return
 			}
 
@@ -148,10 +158,17 @@ func GetAllNodesData(startIndex, endIndex int) ([]NodeData, error) {
 			}
 
 			nodeData[idx].Balance = balance
+			errCh <- nil
 		}(idx, nodeInfo.NodeAddress)
 	}
 
-	wg.Wait()
+	for i := 0; i < len(allNodeInfos); i++ {
+		err := <-errCh
+		if err != nil {
+			return nil, err
+		}
+	}
+	close(errCh)
 
 	return nodeData, nil
 }
