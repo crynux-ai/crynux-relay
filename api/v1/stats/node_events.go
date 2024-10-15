f1ec16b590d2811f084d6c2c1881738cd2d41ba2
iwehf <henry.lee@crynux.ai>
2024-10-15 17:59:50 +0800

    bug fix: fix sql in get node event logs

--- api/v1/stats/node_events.go
+++ api/v1/stats/node_events.go
@@ -15,13 +15,6 @@ import (
 func GetNodeEventLogs(ctx *gin.Context) error {
 	start := time.Now().Add(-time.Hour)
 
-	subquery := config.GetDB().Table("selected_nodes").Select("selected_nodes.id AS id, selected_nodes.node_address AS node_address, inference_tasks.task_id AS task_id, inference_tasks.task_args AS task_args").Joins("INNER JOIN inference_tasks ON selected_nodes.inference_task_id = inference_tasks.id AND inference_tasks.task_type = 0")
-	rows, err := config.GetDB().Table("selected_node_status_logs").Select("selected_node_status_logs.created_at, selected_node_status_logs.status, s.node_address, s.task_id, s.task_args").Joins("(?) s ON selected_node_status_logs.selected_node_id = s.id", subquery).Where("selected_node_status_logs.created_at >= ? AND selected_node_status_logs.status != ?", start, models.NodeStatusRunning).Rows()
-	if err != nil {
-		return response.NewExceptionResponse(err)
-	}
-	defer rows.Close()
-
 	var t time.Time
 	var status models.NodeStatus
 	var nodeAddress string
@@ -33,20 +26,40 @@ func GetNodeEventLogs(ctx *gin.Context) error {
 	var nodeAddresses []string
 	var taskIDs []uint64
 	var taskModels []string
-	for rows.Next() {
-		if err := rows.Scan(&t, &status, &nodeAddress, &taskID, &taskArgs); err != nil {
+
+	limit := 500
+	offset := 0
+	subquery := config.GetDB().Table("selected_nodes").Select("selected_nodes.id AS id, selected_nodes.node_address AS node_address, inference_tasks.task_id AS task_id, inference_tasks.task_args AS task_args").Joins("INNER JOIN inference_tasks ON selected_nodes.inference_task_id = inference_tasks.id AND inference_tasks.task_type = 0")
+	for {
+		rows, err := config.GetDB().Table("selected_node_status_logs").Select("selected_node_status_logs.created_at, selected_node_status_logs.status, s.node_address, s.task_id, s.task_args").Joins("INNER JOIN (?) s ON selected_node_status_logs.selected_node_id = s.id", subquery).Where("selected_node_status_logs.created_at >= ? AND selected_node_status_logs.status != ?", start, models.NodeStatusRunning).Order("selected_node_status_logs.created_at").Offset(offset).Limit(limit).Rows()
+		if err != nil {
 			return response.NewExceptionResponse(err)
 		}
-
-		times = append(times, t)
-		allStatus = append(allStatus, status)
-		nodeAddresses = append(nodeAddresses, nodeAddress)
-		taskIDs = append(taskIDs, taskID)
-		if strings.Contains(taskArgs, "crynux-ai/stable-diffusion-xl-base-1.0") {
-			taskModels = append(taskModels, "SDXL")
-		} else {
-			taskModels = append(taskModels, "SD1.5")
+		defer rows.Close()
+		
+		count := 0
+		for rows.Next() {
+			if err := rows.Scan(&t, &status, &nodeAddress, &taskID, &taskArgs); err != nil {
+				return response.NewExceptionResponse(err)
+			}
+	
+			times = append(times, t)
+			allStatus = append(allStatus, status)
+			nodeAddresses = append(nodeAddresses, nodeAddress)
+			taskIDs = append(taskIDs, taskID)
+			var taskModel string = ""
+			if strings.Contains(taskArgs, "crynux-ai/stable-diffusion-xl-base-1.0") {
+				taskModel = "SDXL"
+			} else if strings.Contains(taskArgs, "crynux-ai/stable-diffusion-v1-5") {
+				taskModel = "SD1.5"
+			}
+			taskModels = append(taskModels, taskModel)
+			count += 1
 		}
+		if count < limit {
+			break
+		}
+		offset += limit
 	}
 
 	var nodeDatas []models.NetworkNodeData
