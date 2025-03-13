190df31110c3ac4f018d2de77716935b1080ddd5
iwehf <henry.lee@crynux.ai>
2025-03-13 10:55:16 +0800

    abort task api

--- api/v1/inference_tasks/abort_task.go
+++ api/v1/inference_tasks/abort_task.go
@@ -1,9 +1,18 @@
 package inference_tasks
 
 import (
+	"crynux_relay/api/v1/response"
+	"crynux_relay/api/v1/validate"
+	"crynux_relay/config"
 	"crynux_relay/models"
+	"crynux_relay/service"
+	"database/sql"
+	"errors"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 )
 
 type AbortTaskInput struct {
@@ -17,6 +26,43 @@ type AbortTaskInputWithSignature struct {
 	Signature string `json:"signature" description:"Signature" validate:"required"`
 }
 
-func AbortTask(_ *gin.Context, input *AbortTaskInputWithSignature) (*TaskResponse, error) {
-	return nil, nil
+func AbortTask(c *gin.Context, in *AbortTaskInputWithSignature) (*response.Response, error) {
+	match, address, err := validate.ValidateSignature(in.AbortTaskInput, in.Timestamp, in.Signature)
+
+	if err != nil || !match {
+
+		if err != nil {
+			log.Debugln("error in sig validate: " + err.Error())
+		}
+
+		validationErr := response.NewValidationErrorResponse("signature", "Invalid signature")
+		return nil, validationErr
+	}
+
+	task, err := models.GetTaskByIDCommitment(c.Request.Context(), config.GetDB(), in.TaskIDCommitment)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			validationErr := response.NewValidationErrorResponse("task_id_commitment", "Task not found")
+			return nil, validationErr
+		} else {
+			return nil, response.NewExceptionResponse(err)
+		}
+	}
+
+	if !(address == task.Creator || address == task.SelectedNode) {
+		return nil, response.NewValidationErrorResponse("signature", "Signer not allowed")
+	}
+
+	if task.StartTime.Valid && task.StartTime.Time.Add(time.Duration(task.Timeout)).Compare(time.Now()) > 0 {
+		return nil, response.NewValidationErrorResponse("task_id_commitment", "Timeout not reached")
+	}
+
+	task.AbortReason = in.AbortReason
+	if !task.ValidatedTime.Valid {
+		task.ValidatedTime = sql.NullTime{Time: time.Now(), Valid: true}
+	}
+	if err := service.SetTaskStatusEndAborted(c.Request.Context(), config.GetDB(), task, address); err != nil {
+		return nil, response.NewExceptionResponse(err)
+	}
+	return &response.Response{}, nil
 }
