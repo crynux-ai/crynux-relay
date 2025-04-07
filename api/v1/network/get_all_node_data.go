e0432dba650d99811c1c8bb2f3a99cf49c3d6bbb
iwehf <henry.lee@crynux.ai>
2025-04-07 12:04:48 +0800

    count node incentives

--- api/v1/network/get_all_node_data.go
+++ api/v1/network/get_all_node_data.go
@@ -4,6 +4,7 @@ import (
 	"crynux_relay/api/v1/response"
 	"crynux_relay/config"
 	"crynux_relay/models"
+	"crynux_relay/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,9 +14,17 @@ type GetAllNodesDataParams struct {
 	Total int `query:"total" json:"total" validate:"required,max=100,min=1"`
 }
 
+type NetworkNodeData struct {
+	Address   string  `json:"address"`
+	CardModel string  `json:"card_model"`
+	VRam      int     `json:"v_ram"`
+	Balance   float64 `json:"balance"`
+	QoS       int64   `json:"qos"`
+}
+
 type GetAllNodesDataResponse struct {
 	response.Response
-	Data []models.NetworkNodeData `json:"data"`
+	Data []NetworkNodeData `json:"data"`
 }
 
 func GetAllNodeData(_ *gin.Context, in *GetAllNodesDataParams) (*GetAllNodesDataResponse, error) {
@@ -24,8 +33,18 @@ func GetAllNodeData(_ *gin.Context, in *GetAllNodesDataParams) (*GetAllNodesData
 	if err := config.GetDB().Model(&models.NetworkNodeData{}).Order("id ASC").Limit(in.Total).Offset(in.Start).Find(&allNodeData).Error; err != nil {
 		return nil, response.NewExceptionResponse(err)
 	}
-
+	var data []NetworkNodeData
+	for _, node := range allNodeData {
+		balance, _ := utils.WeiToEther(&node.Balance.Int).Float64()
+		data = append(data, NetworkNodeData{
+			Address:   node.Address,
+			CardModel: node.CardModel,
+			VRam:      node.VRam,
+			Balance:   balance,
+			QoS:       node.QoS,
+		})
+	}
 	return &GetAllNodesDataResponse{
-		Data: allNodeData,
+		Data: data,
 	}, nil
 }
---
+++ service/incentive.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"context"
+	"crynux_relay/models"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+func addNodeIncentive(ctx context.Context, db *gorm.DB, nodeAddress string, incentive float64) error {
+	dbCtx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	t := time.Now().UTC().Truncate(24 * time.Hour)
+	nodeIncentive := models.NodeIncentive{Time: t, NodeAddress: nodeAddress}
+	if err := db.WithContext(ctx).Model(&nodeIncentive).Where(&nodeIncentive).First(&nodeIncentive).Error; err != nil {
+		if err != gorm.ErrRecordNotFound {
+			return err
+		}
+	}
+	if nodeIncentive.ID > 0 {
+		nodeIncentive.Incentive += incentive
+		nodeIncentive.TaskCount += 1
+		if err := db.WithContext(dbCtx).Save(&nodeIncentive).Error; err != nil {
+			return err
+		}
+	} else {
+		nodeIncentive.Incentive = incentive
+		nodeIncentive.TaskCount = 1
+		if err := db.WithContext(dbCtx).Create(&nodeIncentive).Error; err != nil {
+			return err
+		}
+	}
+	return nil
+}
