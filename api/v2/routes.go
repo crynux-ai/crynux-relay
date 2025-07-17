782c3d14f9a42c0cd282d46825f92cfb8f9e7f2c
iwehf <henry.lee@crynux.ai>
2025-07-17 09:53:00 +0800

    bug fix: change v2 api id

--- api/v2/routes.go
+++ api/v2/routes.go
@@ -17,6 +17,7 @@ func InitRoutes(r *fizz.Fizz) {
 	incentiveGroup := v2g.Group("incentive", "incentive", "incentive statistics related APIs")
 
 	incentiveGroup.GET("/nodes", []fizz.OperationOption{
+		fizz.ID("incentive_nodes_v2"),
 		fizz.Summary("Get nodes with top K incentive"),
 		fizz.Response("400", "validation errors", response.ValidationErrorResponse{}, nil, nil),
 	}, tonic.Handler(incentive.GetNodeIncentive, 200))
@@ -24,6 +25,7 @@ func InitRoutes(r *fizz.Fizz) {
 	networkGroup := v2g.Group("network", "network", "Network stats related APIs")
 
 	networkGroup.GET("/nodes/data", []fizz.OperationOption{
+		fizz.ID("network_nodes_data_v2"),
 		fizz.Summary("Get the info of all the nodes in the network"),
 		fizz.Response("400", "validation errors", response.ValidationErrorResponse{}, nil, nil),
 	}, tonic.Handler(network.GetAllNodeData, 200))
@@ -31,6 +33,7 @@ func InitRoutes(r *fizz.Fizz) {
 	nodeGroup := v2g.Group("node", "node", "Node APIs")
 
 	nodeGroup.GET("/:address", []fizz.OperationOption{
+		fizz.ID("node_get_v2"),
 		fizz.Summary("Get node info"),
 		fizz.Response("400", "validation errors", response.ValidationErrorResponse{}, nil, nil),
 	}, tonic.Handler(nodes.GetNode, 200))
